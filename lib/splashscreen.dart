482358aab2345fa4e87e76e36f25e49e1f5ebead
Carlos Padilla <charlypadillajr@gmail.com>
2022-05-11 23:16:54 -0400

    Creación del splashscreen de la aplicación.

---
+++ lib/splashscreen.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplash extends StatefulWidget {
+  @override
+  State<MySplash> createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      //navigateAfterSeconds: ,
+      title: Text('Face Generator App',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 35, color: Colors.white)),
+      gradientBackground: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          // ignore: prefer_const_literals_to_create_immutables
+          colors: [
+            Color.fromRGBO(138, 35, 135, 1.0),
+            Color.fromRGBO(255, 64, 87, 1.0),
+            Color.fromRGBO(242, 113, 33, 1.0)
+          ]),
+      loaderColor: Colors.white,
+    );
+  }
+}
