6e0cf21b497ea0105db48b73611d531f3dfece6d
Carlos Padilla <charlypadillajr@gmail.com>
2022-08-24 13:33:29 -0400

    Corrección de ejecución
    Actualización de parámetros por actualización de versión

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:human_generator_app/splashscreen.dart';
 
@@ -15,3 +16,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors
-
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:human_generator_app/home.dart';
@@ -25,7 +25,7 @@ class _MySplashState extends State<MySplash> {
           colors: [
             Color.fromRGBO(138, 35, 135, 1.0),
             Color.fromRGBO(255, 64, 87, 1.0),
-            Color.fromRGBO(242, 113, 33, 1.0)
+            Color.fromRGBO(242, 113, 33, 1.0),
           ]),
       loaderColor: Colors.white,
     );
